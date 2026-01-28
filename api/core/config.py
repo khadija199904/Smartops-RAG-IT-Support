1b0a2ea6a421561c3f4095ed87deaea22f85a400
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-01-28 10:24:07 +0100

    SRA-1: Ingestion PDF et configuration des variables d'environnement

---
+++ api/core/config.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import os
+
+
+# Load environment variables from .env
+load_dotenv()
+
+
+PDF_PATH = os.getenv("PDF_PATH")
+
+
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+if not DATABASE_URL :
+    # Fetch variables
+     USER = os.getenv("user")
+     PASSWORD = os.getenv("password")
+     HOST = os.getenv("host")
+     PORT = os.getenv("port")
+     DBNAME = os.getenv("dbname")
+
+
+     # Construct the SQLAlchemy connection string
+     DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+
+ # Configuration de JWT
+SECRET_KEY = os.getenv("SECRET_KEY")
