408536c5dcba9a3051292f714400947dfbc2d9c2
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-01-31 00:39:02 +0100

    SRA-1 Modifier ingestion.py

--- pipelineRAG/ingestion.py
+++ pipelineRAG/ingestion.py
@@ -4,11 +4,11 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from api.core.config import PDF_PATH
 import os 
 
-
-def ingestion_preparation(file_path=PDF_PATH):
+file_path = PDF_PATH
+def ingestion_preparation():
   if not os.path.exists(file_path):
     raise FileNotFoundError(f"Erreur : Le fichier n'existe pas à l'emplacement : {file_path}")
-  
+  print(f"--- Début de l'ingestion : {PDF_PATH.name} ---")
   try : 
        # charger le PDF
       loader = PyPDFLoader(file_path)
@@ -16,19 +16,19 @@ def ingestion_preparation(file_path=PDF_PATH):
 
        # split documments into cunks
       text_splitter = RecursiveCharacterTextSplitter(
-      chunk_size=1000,  
-      chunk_overlap=100,  
+      chunk_size=500,  
+      chunk_overlap=80,  
       separators=["\n\n", "\n", " ", ""]  
          )
       chunks = text_splitter.split_documents(docs)
-      
+      print(f"Nombre de chunks créés : {len(chunks)}")
 
       return chunks
   except Exception as e:
       raise Exception(f"Une erreur est survenue lors de l'ingestion : {e}")
   
 if __name__ == "__main__":      
-   chunks = ingestion_preparation(file_path=PDF_PATH)
+   chunks = ingestion_preparation()
    print(f"Nombre de chunks créés : {len(chunks)}")
    sample_chunk = chunks[0]
    print("\n--- Exemple de métadonnées du premier chunk ---")
