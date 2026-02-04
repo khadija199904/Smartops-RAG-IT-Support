5566cb2fb27dd80fdeb767eb17c8ba721a0c79c5
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-04 17:33:19 +0100

    SRA-3: remove fle unitile

--- pipelineRAG/rag_chain.py
+++
@@ -1,9 +0,0 @@
-
-from pipelineRAG.ingestion import ingestion_preparation
-from pipelineRAG.vectorstore import create_and_store_embeddings
-
-
-
-
-chunks = ingestion_preparation()
-vecteur = create_and_store_embeddings(chunks)
