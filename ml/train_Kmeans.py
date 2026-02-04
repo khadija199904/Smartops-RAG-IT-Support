8d65c9abf95e125d813236303ea82fcda8845cfa
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-04 11:14:44 +0100

    SRA-3 : Modifier le csript de ntrainnement KMeans

--- ml/train_Kmeans.py
+++ ml/train_Kmeans.py
@@ -1,36 +1,50 @@
+import os
+import joblib
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from pipelineRAG.vectorstore import get_embedding_model
-import numpy as np
-import joblib
+from  api.models.queries import Query
 
-MODEL_SAVE_PATH = "ml/models_saved"
+MODEL_PATH = "ml/models_saved/kmeans_it_support.joblib"
 
-def train_kmeans ():
+def train_kmeans():
     print("--- Phase 1 : Entraînement sur données IT ---")
     
-    with open("./data/it_support_questions.txt", "r", encoding="utf-8") as file_it:
+    # 1. Chargement des données
+    file_path = "./data/it_support_questions.txt"
+    if not os.path.exists(file_path):
+        print(f"Erreur : Le fichier {file_path} est introuvable.")
+        return None
+
+    with open(file_path, "r", encoding="utf-8") as file_it:
         questions = [line.strip() for line in file_it if line.strip()]
 
-   
+    # 2. Génération des embeddings
+     # print(f"Génération des embeddings pour {len(questions)} questions...")
     model_embedding = get_embedding_model()
-
     questions_embeddings = model_embedding.embed_documents(questions)
-
     X = np.array(questions_embeddings)
 
-    # Entraîner KMeans
-    kmeans = KMeans(n_clusters=10, random_state=42)
-    
-    clusters = kmeans.fit(X)
+    # 3. Entraînement KMeans
+   
+    # print("Calcul des clusters...")
+    kmeans = KMeans(n_clusters=5, random_state=42, n_init=10)
+    kmeans.fit(X)
 
-    cluster_ids = kmeans.labels_ 
-    print(f"Questions clustérisées en {len(set(cluster_ids))} groupes")
+    # cluster_ids = kmeans.labels_ 
+    # print(f"Succès : {len(set(cluster_ids))} groupes identifiés.")
     
-
-    joblib.dump(kmeans, MODEL_SAVE_PATH)
-    print(f"Modèle sauvegardé dans {MODEL_SAVE_PATH}")
+    # 4. Sauvegarde du modèle
+    os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+    joblib.dump(kmeans, MODEL_PATH)
+    print(f"Modèle entraîné et sauvegardé .")
     return kmeans
+
     
+    
+
+
+
 if __name__ == "__main__":
-    
     train_kmeans()
